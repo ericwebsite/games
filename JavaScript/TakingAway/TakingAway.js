c79a0ec946f57fd9dd8277f1086ba69756f36d5f
Eric Wang <eric.wang.us@hotmail.com>
2021-11-13 12:50:48 -0800

    Add taking away game

---
+++ JavaScript/TakingAway/TakingAway.js
@@ -0,0 +1,37 @@
+var scene = 1;
+var grid = {};
+var gridLine = {};
+
+var scene = 1;
+var normalLevel = 1;
+var xAni = 0;
+
+function setup(){
+    createCanvas(601, 601);
+    for(var i = 0; i < 20; i++){
+       gridLine[i] = color(0, 0, 0);
+    }
+    for(var i = 0; i < 20; i++){
+      grid[i] = gridLine;
+    }
+    scene = 1;
+    normalLevel = 1;
+}
+
+function draw(){
+    if(scene == 1){
+        fill(0, 146, 14);
+        rect(-10, -10, 1000, 1000);
+        fill(128, 64, 0);
+        rect(10, 300, 285, 90);
+        fill(255, 255, 255);
+        textSize(20);
+        text("Play! ", 130, 350);
+        if(mouseX > 10 && mouseX < 295 && mouseY > 300 && mouseY < 390 && mouseIsPressed){
+            scene = 2;
+        }
+    }else if(scene == 2){
+        fill(2, 2, 2);
+        rect(-1, -1, 100, 100)
+    }
+}
